32b3868b05d67854b5b623a74b802febc5f7f8cd
mravikumarkru <30920491+mravikumarkru@users.noreply.github.com>
2017-08-12 17:20:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/Page1', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'Page1.html'));
+app.get('/page1', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
