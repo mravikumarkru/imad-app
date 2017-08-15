a686bf9b16338e504a78003cb7b30a9cdf706148
mravikumarkru <30920491+mravikumarkru@users.noreply.github.com>
2017-08-15 13:12:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,13 @@ var img = document.getElementById('madi');
 img.onclick = function() {
     img.style.marginLeft = "100px";
     };
+    
+    conter = 0;
+    var button = document.getElementById("counter");
+    button.onclick = function()
+    {
+        counter = counter + 1;
+        var span = document.getElementById("count");
+        span.innerHTML = counter.toString();
+        
+    };
